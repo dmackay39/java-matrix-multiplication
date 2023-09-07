1d4dab1a59f35509fe5af9eb573ae565ffc47fb9
Daniel MacKay <84335791+dmackay39@users.noreply.github.com>
2023-09-07 11:45:49 +0100

    Added multiplication function and UI

--- java-matrix-multiplication/src/main/java/org/example/Main.java
+++ java-matrix-multiplication/src/main/java/org/example/Main.java
@@ -1,7 +1,51 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
+        Scanner myScanner = new Scanner(System.in);
+        System.out.println("Please enter the row size of the first matrix");
+        int rowSize = myScanner.nextInt();
+        myScanner.nextLine();
+        System.out.println("Please enter the column size of the first matrix");
+        int colSize1 = myScanner.nextInt();
+        myScanner.nextLine();
+        System.out.println("Please enter the column size of the second matrix");
+        int colSize2 = myScanner.nextInt();
+        myScanner.nextLine();
+
+        MatrixMultiplier calc = new MatrixMultiplier();
+        calc.setMatrixSize(rowSize,colSize1,colSize2);
+
+        System.out.println("For matrix 1");
+        for (int i =0; i < rowSize; i++){
+            for (int j = 0; j < colSize1; j++){
+                System.out.println("Please enter the " + (i+1) + ", " + (j+1) + "th entry of the matrix");
+                int entry = myScanner.nextInt();
+                calc.setMatrix1Element(i,j,entry);
+                myScanner.nextLine();
+            }
+        }
+
+        System.out.println("For matrix 2");
+        for (int i =0; i < colSize1; i++){
+            for (int j = 0; j < colSize2; j++){
+                System.out.println("Please enter the " + (i+1) + "," + (j+1) + "-th entry of the matrix");
+                int entry = myScanner.nextInt();
+                calc.setMatrix2Element(i,j,entry);
+                myScanner.nextLine();
+            }
+        }
+
+        calc.multiplyMatrices(rowSize,colSize1,colSize2);
+        System.out.println("The answer is:");
+        for (int i=0;i<rowSize;i++){
+            for (int j=0;j<colSize2;j++){
+                System.out.print(calc.getResultMatrixElement(i,j) + " ");
+            }
+            System.out.println();
+        }
     }
 }
--- java-matrix-multiplication/src/main/java/org/example/MatrixMultiplier.java
+++ java-matrix-multiplication/src/main/java/org/example/MatrixMultiplier.java
@@ -5,24 +5,65 @@ public class MatrixMultiplier {
         return matrix1;
     }
 
-    public void setMatrix1(int[][] matrix1) {
-        this.matrix1 = matrix1;
+    public void setMatrix1(int rowSize, int colSize) {
+        this.matrix1 = new int[rowSize][colSize];
     }
 
     public int[][] getMatrix2() {
         return matrix2;
     }
 
-    public void setMatrix2(int[][] matrix2) {
-        this.matrix2 = matrix2;
+    public void setMatrix2(int rowSize, int colSize) {
+        this.matrix2 = new int[rowSize][colSize];
     }
+
+    public void setResultMatrix(int rowSize, int colSize) {
+        this.resultMatrix = new int[rowSize][colSize];
+    }
+
 
     private int[][] matrix2;
 
     private int[][] matrix1;
 
-    public void setMatrixElement(int[][] matrix, int rowIdentifier,
+    private int[][] resultMatrix;
+
+    public void setMatrix1Element(int rowIdentifier,
                                 int columnIdentifier, int element){
-        matrix[rowIdentifier][columnIdentifier] = element;
+        matrix1[rowIdentifier][columnIdentifier] = element;
     }
+
+    public void setMatrix2Element(int rowIdentifier,
+                                  int columnIdentifier, int element){
+        matrix2[rowIdentifier][columnIdentifier] = element;
+    }
+
+    public void setResultMatrixElement(int rowIdentifier,
+                                  int columnIdentifier, int element){
+        resultMatrix[rowIdentifier][columnIdentifier] = element;
+    }
+
+    public int getResultMatrixElement(int rowIdentifier,
+                                       int columnIdentifier){
+        return resultMatrix[rowIdentifier][columnIdentifier];
+    }
+    public void setMatrixSize(int rowSize1, int colSize1, int colSize2){
+        setMatrix1(rowSize1, colSize1);
+        setMatrix2(colSize1, colSize2);
+        setResultMatrix(rowSize1,colSize2);
+    }
+
+    public void multiplyMatrices(int rowSize1, int columnSize1, int columnSize2){
+        for (int i=0; i<rowSize1;i++){
+            for (int j=0; j<columnSize2;j++){
+                int sumEntries = 0;
+                for (int k=0; k<columnSize1;k++){
+                    sumEntries += matrix1[i][k]*matrix2[k][j];
+                }
+                setResultMatrixElement(i,j,sumEntries);
+            }
+        }
+    }
+
+
 }
